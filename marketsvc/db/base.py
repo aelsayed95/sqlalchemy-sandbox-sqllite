a33322695c2d9e667e6d97aad41d673e94a6d760
Aya Elsayed <ayah.ehab11@gmail.com>
2024-05-06 16:50:38 +0100

    Add ORMs

--- marketsvc/db/base.py
+++ marketsvc/db/base.py
@@ -1,3 +1,7 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
 
 engine = create_engine("sqlite+pysqlite:///market.db", echo=True)
+
+class Base(DeclarativeBase):
+    pass
